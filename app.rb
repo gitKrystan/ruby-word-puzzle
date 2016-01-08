ed0746801f0cf0e988d9fe62b51c7a1d05bd0070
Krystan Menne <krystan@yup.local>
2016-01-08 12:03:28 -0800

    add solution page

--- app.rb
+++ app.rb
@@ -8,6 +8,13 @@ get('/') do
 end
 
 post('/puzzle') do
-  @encrypted_puzzle = params.fetch('user_string').make_puzzle()
+  original = params.fetch('user_string')
+  @encrypted_puzzle = original.make_puzzle()
   erb(:puzzle)
+end
+
+post('/solution') do
+  @guess = params.fetch('user_guess')
+  @guess_result = "You are correct!"
+  erb(:solution)
 end
--- spec/word_puzzle_integration_spec.rb
+++ spec/word_puzzle_integration_spec.rb
@@ -13,3 +13,14 @@ describe('the puzzle encryption path', {:type => :feature}) do
       .to(have_content("B-l--v- y-- c-n -nd y--'r- h-lfw-y th-r-. Th--d-r- R--s-v-lt"))
   end
 end
+
+describe('the puzzle decription path', {:type => :feature}) do
+  it('processes the user entry and tells them if their guess is correct') do
+    visit('/')
+    fill_in('user_string', :with => "Hey there!")
+    click_button('Generate Puzzle')
+    fill_in('user_guess', :with => "hey there!")
+    click_button('Am I right?')
+    expect(page).to(have_content('You are correct!'))
+  end
+end
